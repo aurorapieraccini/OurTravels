155549687fee8b86d8adc6ec4afd6ee0f79e8fe1
david-vsc <david207799@yahoo.com>
2025-01-11 16:52:18 +0200

    connected the login feature in ourtravels.html with the internal website

--- django/travels/urls.py
+++ django/travels/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-
+from django.contrib import admin
 from . import views
 
 urlpatterns = [
@@ -9,6 +9,10 @@ urlpatterns = [
     path("ourtravels", views.ourtravels, name="ourtravels"),
     # ex: /polls/5/results/
     path("countries/", views.countries, name="countries"),
+
+    path('login/', views.custom_login, name='custom_login'),
+    path('register/', views.register, name='register'),  # Optional for registration
+    #path('admin/', admin.site.urls),  # Django admin URL
     # ex: /polls/5/vote/
     # path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
--- django/travels/views.py
+++ django/travels/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import Http404
 from .models import *
-  
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from django.contrib.auth.models import User
+
 # Create your views here.
 
 # def country(request, country_id):
@@ -18,3 +21,27 @@ def countries(request):
 #     return render(request, "travels/countries.html", {"countries": countries})
 def ourtravels(request):
     return render(request, 'travels/ourtravels.html')
+
+def custom_login(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            # Redirect to Django admin
+            return redirect('/admin/')
+        else:
+            messages.error(request, "Invalid username or password")
+    return redirect('/')  # Redirect to the homepage or wherever you want
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "Username already exists")
+        else:
+            User.objects.create_user(username=username, password=password)
+            messages.success(request, "Registration successful")
+    return redirect('/')  # Redirect to the homepage or wherever you want
