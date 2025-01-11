86374651eed4a281e70e9f3adabcea6be73c14d2
david-vsc <david207799@yahoo.com>
2025-01-11 15:54:44 +0200

    created city table

--- django/travels/models.py
+++ django/travels/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+
 # Create your models here.
 class Country(models.Model): 
     name = models.CharField(max_length=64)
@@ -11,10 +12,16 @@ class VisitedCountry(models.Model):
     country = models.ForeignKey(Country, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+class City(models.Model): 
+    name = models.CharField(max_length=168)
+
+    def __str__(self):
+        return self.name
+
 class VisitedCities(models.Model):
-    country = models.ForeignKey(Country, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    country = models.ForeignKey(City, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)        
 
 
     def __str__(self):
-        return "User %s has visited country %s" % (self.user, self.country)
+        return "User %s has visited country %s and city %s " % (self.user, self.country, self.city)
